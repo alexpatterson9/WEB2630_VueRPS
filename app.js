c876d3af7fd2a927b56569a0e652e49d7cea6b6b
Alex Patterson <alexpatterson2@mail.weber.edu>
2022-02-01 16:31:41 -0700

    started

--- app.js
+++ app.js
@@ -1,9 +1,45 @@
 new Vue({
   el: '#app',
   data: {
-
+    playerSelected: "",
+    playerScore: 0,
+    computerSelected: "",
+    computerScore: 0,
+    totalTurns: 10,
+    availableActions: [
+      "rock", 
+      "paper",
+      "scissors",
+    ]
   },
   methods: {
-
+    startNewGame() {
+      this.playerScore = 0;
+      this.computerScore = 0;
+      this.playerSelected = "";
+    },
+    selectAction(action) {
+      this.playerSelected = action;
+      await setTimeout(this.computerTurn, 500);
+      this.displayResult();
+    },
+    computerTurn() {
+      this.computerSelected = this.availableActions[this.getRandomNumber(3)];
+    },
+    displayResult() {
+      console.log(this.playerSelected);
+      console.log(this.computerSelected);
+    },
+    getRandomNumber(max) {
+      return Math.floor(Math.random() * max);
+    }
+  },
+  computed: {
+    playerPercent() {
+      return this.playerScore / this.totalTurns;
+    },
+    computerPercent() {
+      return this.computerScore / this.totalTurns;
+    }
   }
 });
